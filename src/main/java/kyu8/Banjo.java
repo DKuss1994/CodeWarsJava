5cec2a4d4b3a1d8345ccf5050ade7b3f39db085f
DKuss1994 <kussdennis@web.de>
2025-11-21 20:29:25 +0100

    new kata kyu 8

--- src/main/java/kyu8/Banjo.java
+++ src/main/java/kyu8/Banjo.java
@@ -1,4 +1,39 @@
 package kyu8;
 
+/*# Are You Playing Banjo? (8 kyu)
+
+# https://www.codewars.com/kata/53af2b8861023f1d88000832/java
+        # Sprache: Java
+# Datum: 2025-11-21
+
+        # Task: Create a function which answers the question "Are you playing banjo?".
+        # If a name starts with the letter "R" or lowercase "r", the person is playing banjo.
+        # The function takes a name as its only argument, and returns one of the following strings:
+        # - name + " plays banjo"
+        # - name + " does not play banjo"
+        # Names given are always valid strings.
+
+        # Examples:
+        # "Adam" -> "Adam does not play banjo"
+        # "Ringo" -> "Ringo plays banjo"
+        # "rolf" -> "rolf plays banjo"
+        # "bravo" -> "bravo does not play banjo"
+
+# Ich hab gelernt, wie man Strings überprüft, ob sie mit einem bestimmten Buchstaben beginnen, und darauf basierend unterschiedliche Ausgaben erzeugt.
+*/
 public class Banjo {
+    public static String areYouPlayingBanjo(String name) {
+        String namelow = name.toLowerCase();
+        char first = namelow.charAt(0);
+        char search = 'r';
+
+        if (first == search ){
+            return name + " plays banjo";
+        }
+        else{
+            return name+ " does not play banjo";
+        }
+
+    }
+
 }
--- src/test/java/de.dennis.codewars/kyu8/BanjoTest.java
+++ src/test/java/de.dennis.codewars/kyu8/BanjoTest.java
@@ -1,10 +1,34 @@
 package de.dennis.codewars.kyu8;
 
+import kyu8.Banjo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BanjoTest {
+    @Test
+
+    public void BanjoExampleTestFalse() {
+        String name = "Mark";
+        Assertions.assertEquals(name + " does not play banjo", Banjo.areYouPlayingBanjo(name));
+        }
+    @Test
+
+    public void BanjoExampleTestTrue() {
+        String name = "Rieke";
+        Assertions.assertEquals(name + " plays banjo" , Banjo.areYouPlayingBanjo(name));
+        }
+    @Test
+
+    public void BanjoExampleTestFalselower() {
+        String name = "mark";
+        Assertions.assertEquals(name + " does not play banjo", Banjo.areYouPlayingBanjo(name));
+        }
+    @Test
+
+    public void BanjoExampleTestTruelower() {
+        String name = "rieke";
+        Assertions.assertEquals(name + " plays banjo" , Banjo.areYouPlayingBanjo(name));
+        }
+    }
 
 
-    /*@Test
-    public void stringTest(){
-        String text = "Hello";
-        Assertions.assertThat(Main.test(text)).isEqualTo(te  xt);*/
-}
