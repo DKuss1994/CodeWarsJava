011a33317a1cb88e0b2feea68d12947c9d0bca6b
DKuss1994 <kussdennis@web.de>
2025-11-25 20:04:36 +0100

    Misstak. calculation errors. Now all green under 300ms

--- src/main/java/kyu8/TwiceAsOld/TwiceAsOld.java
+++ src/main/java/kyu8/TwiceAsOld/TwiceAsOld.java
@@ -2,34 +2,14 @@ package kyu8.TwiceAsOld;
 
 public class TwiceAsOld {
     public static int twiceAsOld(int dadYears, int sonYears) {
-        int count = 1;
-
-        while (dadYears / 2 != sonYears) {
-            dadYears++;
-            sonYears++;
-            count++;
-
-
+        int value;
+        value = dadYears - 2*sonYears;
+        if (value<0){
+            return value * -1;
         }
-        if(count<0){
-            count *= -1;
-            return count;
+        else{
+            return value;
         }
-        else {
-            if(dadYears%2!= 0){
-                return count++;
-            }
-            else if(dadYears/2 == sonYears){
-                return count = 0;
-
-            }
-            else {
-                return count;
-            }
-
-        }
-
 
     }
-
 }
