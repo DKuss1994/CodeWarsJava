cb4737fd11547b9199c783a1c7c08f424ff10c7e
DKuss1994 <kussdennis@web.de>
2025-11-27 17:46:49 +0100

    TestCase2 green

--- src/main/java/kyu7/Printer/Printer.java
+++ src/main/java/kyu7/Printer/Printer.java
@@ -2,6 +2,43 @@ package kyu7.Printer;
 
 import java.util.ArrayList;
 
+/*# Druckerfehler (7 kyu)
+
+# https://www.codewars.com/kata/56541980fa08ab47a0000040/java
+        # Sprache: Java
+# Datum: 2025-11-21
+
+        # Aufgabe:
+        # In einer Fabrik druckt ein Drucker Etiketten für Boxen.
+        # Für eine Boxenart darf der Drucker nur Farben verwenden, die mit den Buchstaben
+        # von a bis m bezeichnet sind.
+        #
+        # Die verwendeten Farben werden in einem Kontrollstring aufgezeichnet.
+        # Beispiel für einen „guten“ Kontrollstring: "aaabbbbhaijjjm"
+        # - drei Mal 'a', vier Mal 'b', einmal 'h', einmal 'a'...
+        #
+        # Manchmal gibt es Probleme: Farben fehlen oder es gibt technische Störungen,
+        # wodurch ein „schlechter“ Kontrollstring entsteht, z.B. "aaaxbbbbyyhwawiwjjjwwm"
+        # mit Buchstaben außerhalb von a bis m.
+        #
+        # Schreibe eine Funktion printer_error, die den Fehleranteil des Druckers
+        # als String zurückgibt, in der Form "Fehleranzahl/Länge des Strings".
+        # Vereinfache den Bruch nicht.
+        #
+        # Hinweise:
+        # - Der String hat mindestens eine Länge von 1.
+        # - Enthält nur Buchstaben von a bis z.
+
+        # Beispiele:
+        # s = "aaabbbbhaijjjm" -> "0/14"
+        # s = "aaaxbbbbyyhwawiwjjjwwm" -> "8/22"
+
+# Ich hab gelernt, wie man überprüft, welche Buchstaben in einem String zulässig sind,
+# wie man Fehler zählt und sie als Bruch darstellt.
+# Außerdem habe ich verstanden, dass man manchmal ArrayLists nutzen kann,
+# aber es auch kürzer geht, indem man die Buchstaben direkt vergleicht.
+*/
+
 
 public class Printer {
 
@@ -28,6 +65,7 @@ public class Printer {
 
             }
         }
+
 
 
         return counterError + "/" + s.length();
