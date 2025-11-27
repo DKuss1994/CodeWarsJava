538bfdf2c449457a63bdcf7a3d16f38d81be7376
DKuss1994 <kussdennis@web.de>
2025-11-27 17:34:42 +0100

    TestCase2 green

--- src/main/java/kyu7/Printer/Printer.java
+++ src/main/java/kyu7/Printer/Printer.java
@@ -1,11 +1,36 @@
 package kyu7.Printer;
 
+import java.util.ArrayList;
+
+
 public class Printer {
 
     public static String printerError(String s) {
         int counterError = 0;
+        ArrayList<Character> druckerFarbe = new ArrayList<>();
+        druckerFarbe.add('a');
+        druckerFarbe.add('b');
+        druckerFarbe.add('c');
+        druckerFarbe.add('d');
+        druckerFarbe.add('e');
+        druckerFarbe.add('f');
+        druckerFarbe.add('g');
+        druckerFarbe.add('h');
+        druckerFarbe.add('i');
+        druckerFarbe.add('j');
+        druckerFarbe.add('k');
+        druckerFarbe.add('l');
+        druckerFarbe.add('m');
 
-        return counterError +"/"+s.length();
+        for (int i = 0; i < s.length(); i++) {
+            if (!druckerFarbe.contains(s.charAt(i))) {
+                counterError++;
+
+            }
+        }
+
+
+        return counterError + "/" + s.length();
 
     }
 }
