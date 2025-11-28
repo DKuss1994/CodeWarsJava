42a1fdf0c8da090e659e2d2442fa88feaa417651
DKuss1994 <kussdennis@web.de>
2025-11-28 11:55:33 +0100

    TestCase green to Yellow green

--- src/main/java/kyu8/TrafficLights/TrafficLights.java
+++ src/main/java/kyu8/TrafficLights/TrafficLights.java
@@ -3,6 +3,9 @@ package kyu8.TrafficLights;
 public class TrafficLights {
 
     public static String updateLight(String current) {
+        if(current == "green"){
+            return "yellow";
+        }
 
 
 
