6d70b6cc7307e7d46359dfc91eca6ed81d307370
DKuss1994 <kussdennis@web.de>
2025-12-05 21:20:01 +0100

    green  test

--- src/main/java/kyu7/JadenCase/JadenCase.java
+++ src/main/java/kyu7/JadenCase/JadenCase.java
@@ -1,11 +1,39 @@
 package kyu7.JadenCase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 public class JadenCase {
-
+    // hello world
     public String toJadenCase(String phrase) {
-        // TODO put your code below this comment
+        String[] jaden ;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder = new StringBuilder();
+        int count = 1;
 
-        return null;
-    }
+        if (phrase.isEmpty()) {
+            return null;
+        } else {
+            jaden = phrase.split(" ");
+            for (String s : jaden) {
+                stringBuilder.append(s.substring(0,1).toUpperCase(Locale.ROOT));
+                stringBuilder.append(s.substring(1));
+                if(count < jaden.length){
+                    stringBuilder.append(" ");
+                    count++;
+                }
 
-}
+                }
+
+            }
+           return String.valueOf(stringBuilder);
+            }
+
+        }
+
+
+
+
+
--- src/test/java/kyu7/JadenCase/JadenCaseTest.java
+++ src/test/java/kyu7/JadenCase/JadenCaseTest.java
@@ -2,6 +2,7 @@ package kyu7.JadenCase;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class JadenCaseTest {
@@ -11,5 +12,16 @@ public class JadenCaseTest {
     public void testNullArg() {
         assertNull( jadenCase.toJadenCase(null));
     }
+    @Test
+    public void testEmptyArg() {
+        assertNull(jadenCase.toJadenCase(""));
+    }
+    @Test
+    public void testCase(){
+        String x = "How can mirrors be real if our eyes aren't real";
+        String test = "How Can Mirrors Be Real If Our Eyes Aren't Real";
+        assertEquals(jadenCase.toJadenCase(x),test);
+    }
 
 }
+
