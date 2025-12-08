03921672a0be069568a7a8ed976193154b1728c0
DKuss1994 <kussdennis@web.de>
2025-12-08 21:04:36 +0100

    testCaseYes failed we get a true and return String yes

---
+++ src/main/java/kyu8/YesOrNo/YesOrNo.java
@@ -0,0 +1,11 @@
+package kyu8.YesOrNo;
+
+class YesOrNo
+{
+    public static String boolToWord(boolean b)
+    {
+        //TODO
+    return "hello";
+    }
+
+}
---
+++ src/test/java/kyu8/YesOrNo/YesOrNoTest.java
@@ -0,0 +1,13 @@
+package kyu8.YesOrNo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static kyu8.YesOrNo.YesOrNo.boolToWord;
+
+public class YesOrNoTest {
+    @Test
+    public void yesTestCase(){
+        Assertions.assertEquals(boolToWord(true),"yes");
+    }
+}
