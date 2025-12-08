e440f71c8bebe6e35b6fd508f8be9f9d103091eb
DKuss1994 <kussdennis@web.de>
2025-12-08 10:46:03 +0100

    Swtich case add

--- src/main/java/kyu7/ArithmeticFunction/ArithmeticFunction.java
+++ src/main/java/kyu7/ArithmeticFunction/ArithmeticFunction.java
@@ -1,21 +1,37 @@
 package kyu7.ArithmeticFunction;
 
-
-//5, 2, "add"      --> 7
-//5, 2, "subtract" --> 3
-//5, 2, "multiply" --> 10
-//5, 2, "divide"   --> 2.5
 class ArithmeticFunction {
     public static int arithmetic(int a, int b, String operator) {
-        if(operator.equals("add")){
-            return a+b;
-        }
-        else if(operator.equals("subtract")){
-            return a-b;
+        if (operator.equals("add")) {
+            return a + b;
+        } else if (operator.equals("subtract")) {
+            return a - b;
         } else if (operator.equals("multiply")) {
-            return a*b;
+            return a * b;
+
+        } else if (operator.equals("divide")) {
+            return a / b;
 
         }
         return 0;
+    }
+
+    public static int arithmeticSwitch(int a, int b, String operator) {
+        switch (operator) {
+            case "add":
+                return a + b;
+
+            case "subtract":
+                return a - b;
+
+            case "multiply":
+                return a * b;
+
+            case "divide":
+                return a / b;
+
+
+        }
+        return a;
     }
 }
--- src/test/java/kyu7/ArithmeticFunction/ArithmeticFunctionTest.java
+++ src/test/java/kyu7/ArithmeticFunction/ArithmeticFunctionTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static kyu7.ArithmeticFunction.ArithmeticFunction.arithmetic;
+import static kyu7.ArithmeticFunction.ArithmeticFunction.arithmeticSwitch;
 
 class ArithmeticFunctionTest {
     @Test
@@ -24,5 +25,31 @@ class ArithmeticFunctionTest {
         Assertions.assertEquals(arithmetic(20,8,"multiply"),160);
         Assertions.assertEquals(arithmetic(-5,-5,"multiply"),25);
         Assertions.assertEquals(arithmetic(-10,5,"multiply"),-50);
+    }@Test
+    public void testCaseDivide(){
+        Assertions.assertEquals(arithmetic(5,5,"divide"),1);
+    }
+}class ArithmeticFunctionTestSwitch {
+    @Test
+    public void testCaseAdd(){
+        Assertions.assertEquals(arithmeticSwitch(5,5,"add"),10);
+        Assertions.assertEquals(arithmeticSwitch(20,78,"add"),98);
+        Assertions.assertEquals(arithmeticSwitch(5,-5,"add"),0);
+        Assertions.assertEquals(arithmeticSwitch(-10,5,"add"),-5);
+    }@Test
+    public void testCaseSubtract(){
+        Assertions.assertEquals(arithmeticSwitch(5,5,"subtract"),0);
+        Assertions.assertEquals(arithmeticSwitch(20,78,"subtract"),-58);
+        Assertions.assertEquals(arithmeticSwitch(5,-5,"subtract"),10);
+        Assertions.assertEquals(arithmeticSwitch(-10,5,"subtract"),-15);
+    }@Test
+    public void testCaseMultiply(){
+        Assertions.assertEquals(arithmeticSwitch(5,5,"multiply"),25);
+        Assertions.assertEquals(arithmeticSwitch(20,8,"multiply"),160);
+        Assertions.assertEquals(arithmeticSwitch(-5,-5,"multiply"),25);
+        Assertions.assertEquals(arithmeticSwitch(-10,5,"multiply"),-50);
+    }@Test
+    public void testCaseDivide(){
+        Assertions.assertEquals(arithmeticSwitch(5,5,"divide"),1);
     }
 }
