06fcb47148087fc91b1f0081aabba8689ffa6891
DKuss1994 <kussdennis@web.de>
2025-12-08 10:24:30 +0100

    new kyu7 ArithmeticFunction test case red add

---
+++ src/main/java/kyu7/ArithmeticFunction/ArithmeticFunction.java
@@ -0,0 +1,12 @@
+package kyu7.ArithmeticFunction;
+
+
+//5, 2, "add"      --> 7
+//5, 2, "subtract" --> 3
+//5, 2, "multiply" --> 10
+//5, 2, "divide"   --> 2.5
+class ArithmeticFunction {
+    public static int arithmetic(int a, int b, String operator) {
+        return 5;
+    }
+}
---
+++ src/test/java/kyu7/ArithmeticFunction/ArithmeticFunctionTest.java
@@ -0,0 +1,16 @@
+package kyu7.ArithmeticFunction;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static kyu7.ArithmeticFunction.ArithmeticFunction.arithmetic;
+
+class ArithmeticFunctionTest {
+    @Test
+    public void testCaseAdd(){
+        Assertions.assertEquals(arithmetic(5,5,"add"),10);
+        Assertions.assertEquals(arithmetic(20,78,"add"),98);
+        Assertions.assertEquals(arithmetic(5,-5,"add"),0);
+        Assertions.assertEquals(arithmetic(-10,5,"add"),5);
+    }
+}
