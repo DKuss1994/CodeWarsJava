8e2cdb6db345cae7a5da669bfab70e2032608293
DKuss1994 <kussdennis@web.de>
2025-12-08 21:09:40 +0100

    test case Yes is passed

--- src/main/java/kyu8/YesOrNo/YesOrNo.java
+++ src/main/java/kyu8/YesOrNo/YesOrNo.java
@@ -2,10 +2,14 @@ package kyu8.YesOrNo;
 
 class YesOrNo
 {
-    public static String boolToWord(boolean b)
-    {
-        //TODO
-    return "hello";
+    public static String boolToWord(boolean b) {
+        // We check the parameter and return yes or no-
+        if(b){
+            return "yes";
+        }
+        else {
+            return "no";
+        }
     }
 
 }
