86c4e766f19dc503953096a083572a5cc2372d96
DKuss1994 <kussdennis@web.de>
2025-12-08 11:25:05 +0100

    Switch case soulotion

--- src/main/java/kyu8/Dinglemouse/Dinglemouse.java
+++ src/main/java/kyu8/Dinglemouse/Dinglemouse.java
@@ -18,4 +18,23 @@ public class Dinglemouse {
         }
         return new int[]{human, cat, dog};
     }
+
+
+    public static int[] humanYearsCatYearsDogYearsSwitch(final int humanYears) {
+        int dog = 0; int cat = 0;
+        switch (humanYears){
+            default:
+                cat = 4*(humanYears-2);
+                dog = 5*(humanYears-2);
+
+            case 2:
+                cat+= 9;
+                dog+=9;
+            case 1:
+                cat += 15;
+                dog += 15;
+        }
+
+        return new int[]{humanYears, cat, dog};
+    }
 }
--- src/test/java/kyu8/Dinglemouse/DinglemouseTest.java
+++ src/test/java/kyu8/Dinglemouse/DinglemouseTest.java
@@ -23,3 +23,18 @@ public class DinglemouseTest {
         assertArrayEquals(new int[]{10,56,64}, Dinglemouse.humanYearsCatYearsDogYears(10));
     }
 }
+class DinglemouseTestSwitch {
+    @Test
+    public void one() {
+        assertArrayEquals(new int[]{1,15,15}, Dinglemouse.humanYearsCatYearsDogYearsSwitch(1));
+    }
+    @Test
+    public void two() {
+        assertArrayEquals(new int[]{2,24,24}, Dinglemouse.humanYearsCatYearsDogYearsSwitch(2));
+    }
+
+    @Test
+    public void ten() {
+        assertArrayEquals(new int[]{10,56,64}, Dinglemouse.humanYearsCatYearsDogYearsSwitch(10));
+    }
+}
