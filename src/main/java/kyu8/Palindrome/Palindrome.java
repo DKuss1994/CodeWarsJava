776a0e4e1c2f70b1a8d27eeb3ad9b3cf1a19a699
DKuss1994 <kussdennis@web.de>
2025-12-17 11:22:30 +0100

    Test passed

--- src/main/java/kyu8/Palindrome/Palindrome.java
+++ src/main/java/kyu8/Palindrome/Palindrome.java
@@ -1,8 +1,13 @@
 package kyu8.Palindrome;
 
+import java.util.Locale;
+
 public class Palindrome {
     public static Boolean isPalindrome(String x) {
-        // your code here
-        return true;
+        String text = x.toLowerCase(Locale.ROOT);
+        StringBuilder stringBuilder = new StringBuilder(text);
+        String textChange = stringBuilder.reverse().toString();
+        return text.equals(textChange);
+
     }
 }
--- src/test/java/kyu8/Palindrome/PalindromeTest.java
+++ src/test/java/kyu8/Palindrome/PalindromeTest.java
@@ -8,5 +8,18 @@ public class PalindromeTest {
     void truePalindrom(){
         String palindrome = "a";
         Assertions.assertTrue(Palindrome.isPalindrome(palindrome));
+        palindrome = "aba";
+        Assertions.assertTrue(Palindrome.isPalindrome(palindrome));
+        palindrome = "Abba";
+        Assertions.assertTrue(Palindrome.isPalindrome(palindrome));
+    }
+    @Test
+    void falsePalindrom(){
+        String palindrome ="hello";
+        Assertions.assertFalse(Palindrome.isPalindrome(palindrome));
+        palindrome ="DENNis";
+        Assertions.assertFalse(Palindrome.isPalindrome(palindrome));
+    palindrome ="MAX";
+        Assertions.assertFalse(Palindrome.isPalindrome(palindrome));
     }
 }
