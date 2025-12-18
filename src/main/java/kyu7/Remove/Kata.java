ab59429f03ed40842cfaa14fad75a90dc3dfe6e4
DKuss1994 <kussdennis@web.de>
2025-12-18 10:15:42 +0100

    split and Stringbuilder

--- src/main/java/kyu7/Remove/Kata.java
+++ src/main/java/kyu7/Remove/Kata.java
@@ -2,6 +2,19 @@ package kyu7.Remove;
 
 public class Kata {
     public static String removeUrlAnchor(String url) {
-        return ""; // Write code here
+        int index = url.indexOf('#');
+        if (index == -1) {
+            return url;
+        } else {
+            StringBuilder newLink = new StringBuilder();
+            char[] urlCharArray = url.toCharArray();
+            for (int i = 0; i < index; i++) {
+                newLink.append(urlCharArray[i]);
+            }
+            return newLink.toString();
+        }
+    }
+    public static String removeUrlSplit (String url){
+        return url.split("#")[0];
     }
 }
--- src/test/java/kyu7/Remove/KataTest.java
+++ src/test/java/kyu7/Remove/KataTest.java
@@ -6,9 +6,24 @@ import org.junit.jupiter.api.Test;
 public class KataTest {
 
     @Test
-    void urlTest(){
+    void urlRemoveTest(){
         String url = "www.hallo.de#test";
         String changeURL = "www.hallo.de";
         Assertions.assertEquals(changeURL,Kata.removeUrlAnchor(url));
+    } @Test
+    void urlTest(){
+        String url = "www.hallo.de.test";
+        String changeURL = "www.hallo.de.test";
+        Assertions.assertEquals(changeURL,Kata.removeUrlAnchor(url));
+    }@Test
+    void urlRemoveSplitTest(){
+        String url = "www.hallo.de#test";
+        String changeURL = "www.hallo.de";
+        Assertions.assertEquals(changeURL,Kata.removeUrlSplit(url));
+    } @Test
+    void urlSplitTest(){
+        String url = "www.hallo.de.test";
+        String changeURL = "www.hallo.de.test";
+        Assertions.assertEquals(changeURL,Kata.removeUrlSplit(url));
     }
 }
