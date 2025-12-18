12e48214a67aa30827cb314f9dd816bbcdc9eb04
DKuss1994 <kussdennis@web.de>
2025-12-18 10:16:49 +0100

    Add readme

--- src/main/java/kyu7/Remove/Kata.java
+++ src/main/java/kyu7/Remove/Kata.java
@@ -1,4 +1,37 @@
 package kyu7.Remove;
+/*
+# Remove anchor from URL (7 kyu)
+
+# https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/java
+# Sprache: Java
+# Datum: 2025-12-17
+
+# Aufgabe:
+# Eine URL soll so verändert werden, dass alles nach einem Ankerzeichen # entfernt wird.
+# Existiert kein # in der URL, soll sie unverändert zurückgegeben werden.
+
+# Beispiele:
+# "www.codewars.com#about" -> "www.codewars.com"
+# "www.codewars.com?page=1" -> "www.codewars.com?page=1"
+
+# Lösung 1 removeUrlAnchor:
+# Zuerst wird geprüft, ob das Zeichen # überhaupt existiert.
+# Falls nicht, wird die ursprüngliche URL direkt zurückgegeben.
+# Falls doch, wird die Position des # ermittelt.
+# Anschließend wird Zeichen für Zeichen bis zu dieser Position
+# in einen StringBuilder übernommen.
+
+# Lösung 2 removeUrlSplit:
+# Die URL wird am Zeichen # gesplittet.
+# Der erste Teil des Arrays ist immer der gewünschte URL Teil.
+# Diese Variante ist deutlich kürzer und gut lesbar.
+
+# Ich habe gelernt:
+# wie man indexOf sinnvoll zur Prüfung verwendet
+# wie man Strings manuell mit StringBuilder zusammensetzt
+# wie einfach split für solche Aufgaben genutzt werden kann
+# dass mehrere Lösungswege existieren und Lesbarkeit oft gewinnt
+*/
 
 public class Kata {
     public static String removeUrlAnchor(String url) {
