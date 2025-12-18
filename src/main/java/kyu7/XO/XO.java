95910e0174734e19fd58ce870c359033d7e71557
DKuss1994 <kussdennis@web.de>
2025-12-18 10:46:39 +0100

    test green

--- src/main/java/kyu7/XO/XO.java
+++ src/main/java/kyu7/XO/XO.java
@@ -2,9 +2,22 @@ package kyu7.XO;
 
 public class XO {
 
-    public static boolean getXO (String str) {
-
-        return true;
+    public static boolean getXO(String str) {
+        int x = 0;
+        int o = 0;
+        char xChar = 'x';
+        char oChar = 'o';
+        str = str.toLowerCase();
+        char[] strChar = str.toCharArray();
+        for (char c : strChar) {
+            if (c == xChar) {
+                x++;
+            }
+            if (c == oChar) {
+                o++;
+            }
+        }
+        return x - o == 0;
 
     }
 }
