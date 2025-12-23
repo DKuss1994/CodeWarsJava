fc1213f17f660ac2e81c4bb487411fdede7b1e6d
DKuss1994 <kussdennis@web.de>
2025-12-23 15:51:41 +0100

    testCase go green and Kata solved

--- src/main/java/kyu6/Equal_Sides/Kata.java
+++ src/main/java/kyu6/Equal_Sides/Kata.java
@@ -2,7 +2,30 @@ package kyu6.Equal_Sides;
 
 public class Kata {
     public static int findEvenIndex(int[] arr) {
-        // your code
-        return -1;
+        int index = -1;
+
+        while (true){
+            index++;
+            int sumLeft = 0;
+            int sumRight = 0;
+
+
+        for (int i = index; i < arr.length; i++) {
+            sumRight+= arr[i];
+
+
+        }
+        for (int i = index; i >= 0; i--) {
+            sumLeft += arr[i];
+        }
+        if (sumLeft==sumRight){
+        return index;
+        } else if (index == arr.length-1) {
+            return -1;
+
+        }
+
+
+        }
     }
 }
