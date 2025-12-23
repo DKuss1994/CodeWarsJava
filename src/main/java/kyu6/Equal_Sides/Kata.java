1bcfb143f6275af80b3952bf6f72495131b596ac
DKuss1994 <kussdennis@web.de>
2025-12-23 15:39:56 +0100

    new Kata EqualSides test Failed

---
+++ src/main/java/kyu6/Equal_Sides/Kata.java
@@ -0,0 +1,8 @@
+package kyu6.Equal_Sides;
+
+public class Kata {
+    public static int findEvenIndex(int[] arr) {
+        // your code
+        return -1;
+    }
+}
---
+++ src/test/java/kyu6/Equal_Sides/KataTest.java
@@ -0,0 +1,20 @@
+package kyu6.Equal_Sides;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class KataTest {
+    @Test
+    public void testCase1(){
+        int [] x = {1,2,3,4,3,2,1};
+        Assertions.assertEquals(3,Kata.findEvenIndex(x));
+
+    }
+    @Test
+    public void testCaseFalse(){
+        int [] x = {1,2,3,4,5,6};
+        Assertions.assertEquals(-1,Kata.findEvenIndex(x));
+
+    }
+}
