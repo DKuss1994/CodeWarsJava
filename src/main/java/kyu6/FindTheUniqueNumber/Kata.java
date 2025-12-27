5278d1d2282e5bdf22251ffad43a011f296b5369
DKuss1994 <kussdennis@web.de>
2025-12-27 10:24:01 +0100

    test passed.

--- src/main/java/kyu6/FindTheUniqueNumber/Kata.java
+++ src/main/java/kyu6/FindTheUniqueNumber/Kata.java
@@ -1,9 +1,29 @@
 package kyu6.FindTheUniqueNumber;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Kata {
     public static double findUniq(double arr[]) {
-        // Do the magic
-        return arr[0];
+        double value = 0;
+        //boxed wandelt primeteive Daten Typen in ein Objekt hier Double
+        ArrayList<Double> doubleArrayList = Arrays.stream(arr).boxed().collect(Collectors.toCollection(ArrayList::new));
+        int count;
+        for (double v : doubleArrayList) {
+            count = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (v == arr[i]) {
+                    count++;
+                }
+                if (count == 1) {
+                    value = doubleArrayList.get(i);
+                }
+            }
+        }
+        return value;
+
     }
 }
