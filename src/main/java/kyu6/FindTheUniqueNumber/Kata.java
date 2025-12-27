d6c0f1d7693363f55241bf343d21684690072a2c
DKuss1994 <kussdennis@web.de>
2025-12-27 10:49:07 +0100

    we change it to a map so that we go 1x map

--- src/main/java/kyu6/FindTheUniqueNumber/Kata.java
+++ src/main/java/kyu6/FindTheUniqueNumber/Kata.java
@@ -3,28 +3,21 @@ package kyu6.FindTheUniqueNumber;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Kata {
     public static double findUniq(double arr[]) {
-        double value = 0;
-        //boxed wandelt primeteive Daten Typen in ein Objekt hier Double
-        ArrayList<Double> doubleArrayList = Arrays.stream(arr).boxed().collect(Collectors.toCollection(ArrayList::new));
-        int count;
-        for (double v : doubleArrayList) {
-            count = 0;
-
-            for (int i = 0; i < arr.length; i++) {
-                if (v == arr[i]) {
-                    count++;
-                }
-
-            }
-            if (count == 1) {
-                value = v;
+        Map<Double,Integer> counts = new HashMap<>();
+        for (double v : arr) {
+            counts.put(v,counts.getOrDefault(v,0)+1);
+        }
+        for(Map.Entry<Double,Integer> entry:counts.entrySet()){
+            if(entry.getValue()== 1){
+                return entry.getKey();
             }
         }
-        return value;
-
+return 0.0;
     }
 }
