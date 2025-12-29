e9caef8a8fe64ea90be41480bac966104d6db25d
DKuss1994 <kussdennis@web.de>
2025-12-29 22:57:48 +0100

    test passed

--- src/main/java/kyu6/SmileFaces/SmileFaces.java
+++ src/main/java/kyu6/SmileFaces/SmileFaces.java
@@ -7,14 +7,20 @@ public class SmileFaces {
     public static int countSmileys(List<String> arr) {
         int count = 0;
         for (String s : arr) {
-            char [] smile = s.toCharArray();
-            if (smile[0]==':' ||smile[0]== ';' ){
-                if (smile[1]=='-' ||smile[1]== '~' ){
-                    if(smile[2]=='D' ||smile[2]== ')' ){
+            char[] smile = s.toCharArray();
+            if (smile.length == 2) {
+                if (smile[0] == ':' || smile[0] == ';') {
+                    if (smile[1] == ')' || smile[1] == 'D') {
                         count++;
                     }
-                } else if (smile[1]==')' ||smile[1]== 'D' ) {
-                    count++;
+                }
+            } else if (smile.length == 3) {
+                if (smile[0] == ':' || smile[0] == ';') {
+                    if (smile[1] == '-' || smile[1] == '~') {
+                        if (smile[2] == 'D' || smile[2] == ')') {
+                            count++;
+                        }
+                    }
                 }
             }
         }
