7f4acdee233c4797cdc25d1eae03da92b033d6bd
DKuss1994 <kussdennis@web.de>
2025-12-29 22:34:44 +0100

    Test passed

--- src/main/java/kyu8/MixedSum/MixedSum.java
+++ src/main/java/kyu8/MixedSum/MixedSum.java
@@ -2,12 +2,11 @@ package kyu8.MixedSum;
 
 import java.util.List;
 
+
 public class MixedSum {
 
-    /*
-     * Assume input will be only of Integer o String type
-     */
     public static int sum(List<?> mixed) {
-        return -1;
+         return mixed.stream().map(Object::toString).mapToInt(Integer::parseInt).sum();
+
     }
 }
