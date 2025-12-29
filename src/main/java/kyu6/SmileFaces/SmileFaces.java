726fbc37cb87f8daf9bb7a353e05b6439873ceaa
DKuss1994 <kussdennis@web.de>
2025-12-29 23:08:10 +0100

    add Stream

--- src/main/java/kyu6/SmileFaces/SmileFaces.java
+++ src/main/java/kyu6/SmileFaces/SmileFaces.java
@@ -1,6 +1,55 @@
 package kyu6.SmileFaces;
 
 import java.util.List;
+/*# Count the Smiley Faces! (6 kyu)
+
+# https://www.codewars.com/kata/583203e6eb35d7980400002a/train/java
+# Sprache: Java
+# Datum: 2025-12-27
+
+# Aufgabe:
+# Gegeben ist eine Liste von Strings, die mögliche Smileys darstellen.
+# Die Aufgabe ist es, zu zählen, wie viele dieser Strings gültige
+# lächelnde Gesichter sind.
+
+# Regeln für gültige Smileys:
+# - Die Augen müssen entweder : oder ; sein
+# - Eine Nase ist optional und darf - oder ~ sein
+# - Der Mund muss ) oder D sein
+# - Es dürfen keine anderen Zeichen enthalten sein
+# - Die Reihenfolge ist immer Augen, optional Nase, Mund
+
+# Die Funktion soll:
+# - jeden String einzeln prüfen
+# - zwischen Smileys mit und ohne Nase unterscheiden
+# - nur gültige Smileys zählen
+# - bei einer leeren Liste 0 zurückgeben
+
+# Beispiele:
+# Eingabe:  [":)", ";(", ";}", ":-D"]
+# Ausgabe:  2
+#
+# Eingabe:  [";D", ":-(", ":-)", ";~)"]
+# Ausgabe:  3
+#
+# Eingabe:  [";]", ":[", ";*", ":$", ";-D"]
+# Ausgabe:  1
+
+# Erweiterung:
+# Zusätzlich habe ich eine Lösung mit Java Streams umgesetzt.
+# Dabei wird:
+# - über die Liste gestreamt
+# - mit einer Regular Expression geprüft ob der Smiley gültig ist
+# - die Anzahl der Treffer gezählt
+
+# Ich habe gelernt:
+# - wie man Strings in einzelne Zeichen zerlegt
+# - wie man Zeichen gezielt vergleicht
+# - wie man verschachtelte Bedingungen logisch sauber aufbaut
+# - wie man Regular Expressions zur Validierung einsetzt
+# - wie man Java Streams mit filter und count verwendet
+# - wie man imperativen und funktionalen Code miteinander vergleicht
+*/
 
 public class SmileFaces {
 
@@ -26,6 +75,8 @@ public class SmileFaces {
         }
         return count;
     }
+    public static int countSmileysStream(List<String> arr){
+        return (int) arr.stream().filter(x -> x.matches("[:;][-~]?[D)]")).count();
+    }
 }
 
-// : or ;    - or ~  ) or D
--- src/test/java/kyu6/SmileFaces/SmileFacesTest.java
+++ src/test/java/kyu6/SmileFaces/SmileFacesTest.java
@@ -9,4 +9,22 @@ public class SmileFacesTest {
         a.add(":)"); a.add(":D"); a.add(":-}"); a.add(":-()");
         assertEquals(2, SmileFaces.countSmileys(a));
     }
+    @Test
+    public void test2() {
+        List<String> a =  new ArrayList<String>();
+        a.add(":)"); a.add(":)"); a.add("x-]"); a.add(":ox"); a.add(";-("); a.add(";-)"); a.add(";~("); a.add(":~D");
+        assertEquals(4, SmileFaces.countSmileys(a));
+    }
+    @Test
+    public void testStream1() {
+        List<String> a = new ArrayList<String>();
+        a.add(":)"); a.add(":D"); a.add(":-}"); a.add(":-()");
+        assertEquals(2, SmileFaces.countSmileysStream(a));
+    }
+    @Test
+    public void testStream2() {
+        List<String> a =  new ArrayList<String>();
+        a.add(":)"); a.add(":)"); a.add("x-]"); a.add(":ox"); a.add(";-("); a.add(";-)"); a.add(";~("); a.add(":~D");
+        assertEquals(4, SmileFaces.countSmileysStream(a));
+    }
 }
