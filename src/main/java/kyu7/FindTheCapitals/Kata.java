0c5608eec61801c7957343a66597df6fa40a0aae
DKuss1994 <kussdennis@web.de>
2025-12-30 20:23:00 +0100

    not the best passed

--- src/main/java/kyu7/FindTheCapitals/Kata.java
+++ src/main/java/kyu7/FindTheCapitals/Kata.java
@@ -1,8 +1,21 @@
 package kyu7.FindTheCapitals;
 
+import java.util.ArrayList;
+
 public class Kata{
     public static int[] capitals(String s){
-        int[] value = {0};
+        char[] charArray = s.toCharArray();
+        ArrayList<Integer> valueList= new ArrayList<>();
+        for (int i = 0; i<s.length();i++) {
+            if(Character.isUpperCase(charArray[i])){
+                valueList.add(i);
+            }
+        }
+        int [] value = new int[valueList.size()];
+        for (int i = 0; i < valueList.size(); i++) {
+            value[i] = valueList.get(i);
+        }
         return value;
+
     }
 }
