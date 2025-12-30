a6b1a81baa935dde269c9ffe3439ff0745af2836
DKuss1994 <kussdennis@web.de>
2025-12-30 20:25:34 +0100

    add instruction

--- src/main/java/kyu7/FindTheCapitals/Kata.java
+++ src/main/java/kyu7/FindTheCapitals/Kata.java
@@ -1,4 +1,31 @@
 package kyu7.FindTheCapitals;
+/*# Find the capitals (7 kyu)
+
+# https://www.codewars.com/kata/539ee3b6757843632d00026b/train/java
+# Sprache: Java
+# Datum: 2025-12-XX
+
+# Aufgabe:
+# Schreibe eine Funktion, die alle Positionen von Großbuchstaben
+# in einem String findet und diese als geordnetes Integer Array zurückgibt.
+#
+# Die Funktion soll:
+# - den String Zeichen für Zeichen durchlaufen
+# - erkennen, ob ein Zeichen ein Großbuchstabe ist
+# - den Index jedes Großbuchstabens speichern
+# - die Indizes in der richtigen Reihenfolge zurückgeben
+#
+# Beispiel:
+# Eingabe:  "CodEWaRs"
+# Ausgabe:  [0, 3, 4, 6]
+#
+# Ich habe gelernt:
+# - wie man Strings in Char Arrays umwandelt
+# - wie man mit Character.isUpperCase Großbuchstaben erkennt
+# - wie man Indizes während einer Schleife korrekt speichert
+# - wie man eine ArrayList in ein int Array umwandelt
+# - wie man Listen nutzt, wenn die Größe des Ergebnisses vorher nicht bekannt ist
+*/
 
 import java.util.ArrayList;
 
