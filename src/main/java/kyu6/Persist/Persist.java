66b84565c29f82f7f59b2c174e2d1b3a87c39fb7
DKuss1994 <kussdennis@web.de>
2025-12-31 21:17:05 +0100

    test passed

--- src/main/java/kyu6/Persist/Persist.java
+++ src/main/java/kyu6/Persist/Persist.java
@@ -1,7 +1,29 @@
 package kyu6.Persist;
 
+
 class Persist {
     public static int persistence(long n) {
-        return 0; // your code
+        int count = 0;
+        final char TONUMBER = '0';
+        String valueString = String.valueOf(n);
+        while (valueString.length() != 1) {
+            int value = 1;
+            char[] valueCharArray = valueString.toCharArray();
+            valueString = multiplicativeTheValue(valueCharArray, value, TONUMBER);
+            count++;
+        }
+
+        return count;
     }
+
+    private static String multiplicativeTheValue(char[] valueCharArray, int value, char TONUMBER) {
+        String valueString;
+        for (int i = 0; i < valueCharArray.length; i++) {
+            value *= (valueCharArray[i] - TONUMBER);
+        }
+        valueString = String.valueOf(value);
+        return valueString;
+    }
+
+
 }
--- src/test/java/kyu6/Persist/PersistTest.java
+++ src/test/java/kyu6/Persist/PersistTest.java
@@ -4,12 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PersistTest {
-   @Test
-    void testZeroDigit(){
-       Assertions.assertEquals(0,Persist.persistence(4));
-   }
-   @Test
-    void testOneDigit(){
-       Assertions.assertEquals(1,Persist.persistence(12));
-   }
+    @Test
+    void testZeroDigit() {
+        Assertions.assertEquals(0, Persist.persistence(4));
+    }
+
+    @Test
+    void testOneDigit() {
+        Assertions.assertEquals(1, Persist.persistence(12));
+    }
+
+
+    @Test
+    void testTwoDigit() {
+        Assertions.assertEquals(3, Persist.persistence(39));
+    }
 }
+
