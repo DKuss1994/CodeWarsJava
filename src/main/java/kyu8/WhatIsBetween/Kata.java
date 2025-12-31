b09cf02abd24aec51db3af09a9600c56d9bc28c6
DKuss1994 <kussdennis@web.de>
2025-12-31 14:46:01 +0100

    testcase 2 with negative numbers passed

--- src/main/java/kyu8/WhatIsBetween/Kata.java
+++ src/main/java/kyu8/WhatIsBetween/Kata.java
@@ -1,9 +1,15 @@
 package kyu8.WhatIsBetween;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Kata {
 
     public static int[] between(int a, int b) {
-        // your code here
-        return null;
+        ArrayList<Integer> value = new ArrayList<>();
+        for (int i = a;i<=b;i++){
+            value.add(i);
+        }
+        return value.stream().mapToInt(i->i).toArray();
     }
 }
--- src/test/java/kyu8/WhatIsBetween/KataTest.java
+++ src/test/java/kyu8/WhatIsBetween/KataTest.java
@@ -12,4 +12,11 @@ public class KataTest {
         int b = 4;
         Assertions.assertArrayEquals(value,Kata.between(a,b));
     }
+    @Test
+    void testCase2(){
+        int[] value = {-2,-1,0,1,2,3,4};
+        int a = -2;
+        int b = 4;
+        Assertions.assertArrayEquals(value,Kata.between(a,b));
+    }
 }
