a50beba5341fed1e9d47e15a2038ae7dcb3cdf0c
DKuss1994 <kussdennis@web.de>
2025-12-31 15:06:36 +0100

    add a Exception

--- src/main/java/kyu8/WhatIsBetween/Kata.java
+++ src/main/java/kyu8/WhatIsBetween/Kata.java
@@ -1,11 +1,13 @@
 package kyu8.WhatIsBetween;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Kata {
 
     public static int[] between(int a, int b) {
+        if(a>b){
+            throw new IllegalArgumentException("a must be smaller equal to b");
+        }
         ArrayList<Integer> value = new ArrayList<>();
         for (int i = a;i<=b;i++){
             value.add(i);
--- src/test/java/kyu8/WhatIsBetween/KataTest.java
+++ src/test/java/kyu8/WhatIsBetween/KataTest.java
@@ -19,4 +19,21 @@ public class KataTest {
         int b = 4;
         Assertions.assertArrayEquals(value,Kata.between(a,b));
     }
+    @Test
+    void testCase3(){
+        int[] value = {5};
+        int a = 5;
+        int b = 5;
+        Assertions.assertArrayEquals(value,Kata.between(a,b));
+    }
+    @Test
+    void testCase4(){
+        int[] value = {-2,-1,0,1,2,3,4};
+        int a = -2;
+        int b = -4;
+
+        IllegalArgumentException thrown = Assertions.assertThrowsExactly(IllegalArgumentException.class,()-> Kata.between(a,b));
+        Assertions.assertEquals("a must be smaller equal to b",thrown.getMessage());
+
+    }
 }
