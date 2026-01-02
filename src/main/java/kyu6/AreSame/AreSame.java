f6d870f5a43e5aae1eb47653789002b62444313c
DKuss1994 <kussdennis@web.de>
2026-01-02 12:54:18 +0100

    all test passed

--- src/main/java/kyu6/AreSame/AreSame.java
+++ src/main/java/kyu6/AreSame/AreSame.java
@@ -1,8 +1,25 @@
 package kyu6.AreSame;
 
+import java.util.Arrays;
+
 public class AreSame {
 
     public static boolean comp(int[] a, int[] b) {
-        return true;
+
+        if(a==null||b==null){
+            return false;
+        }
+        if (a.length == 0 && b.length == 0) {
+            return true;
+        }
+
+        if (a.length != b.length) {
+            return false;
+            } else {
+            return Arrays.equals(Arrays.stream(a).map(n->n*n).sorted().toArray(),Arrays
+                    .stream(b).sorted().toArray());
+
+        }
+
     }
 }
--- src/test/java/kyu6/AreSame/AreSameTest.java
+++ src/test/java/kyu6/AreSame/AreSameTest.java
@@ -9,6 +9,42 @@ public class AreSameTest {
     void testCaseTrue(){
         int [] a = {121, 144, 19, 161, 19, 144, 19, 11};
         int [] b = {121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        Assertions.assertTrue(AreSame.comp(a,b));}
+    @Test
+    void testCaseTrue2(){
+        int [] a = {-121, -144, -19, -161, -19, -144, -19, -11};
+        int [] b = {121, 14641, 20736, 361, 25921, 361, 20736, 361};
         Assertions.assertTrue(AreSame.comp(a,b));
+    }@Test
+    void testCaseTrue3(){
+        int [] a = {};
+        int [] b = {};
+        Assertions.assertTrue(AreSame.comp(a,b));
+    }
+    @Test
+    void testCaseFalse(){
+        int [] a = {121, 144, 19, 161, 19, 144, 19, 12};
+        int [] b = {121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        Assertions.assertFalse(AreSame.comp(a,b));
+    }
+    @Test
+    void testCaseFalse2(){
+        int [] a = {121, 144, 19, 161, 19, 144, 19, 11};
+        int [] b = {12100, 14641, 20736, 361, 25921, 361, 20736, 361};
+        Assertions.assertFalse(AreSame.comp(a,b));
+    }
+
+    @Test
+    void testCaseFalse3(){
+        int [] a = {121, 144, 19, 161, 19, 144, 19};
+        int [] b = {121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        Assertions.assertFalse(AreSame.comp(a,b));
+    }
+
+    @Test
+    void testCaseFalse5(){
+        int [] a = {2,2};
+        int [] b = {4,8};
+        Assertions.assertFalse(AreSame.comp(a,b));
     }
 }
