1945cf4f8f2f8c56a93c3f97eeedce0f32cea495
DKuss1994 <kussdennis@web.de>
2026-01-06 15:03:39 +0100

    passed

--- src/main/java/kyu8/Welcome/Welcome.java
+++ src/main/java/kyu8/Welcome/Welcome.java
@@ -1,8 +1,29 @@
 package kyu8.Welcome;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Welcome {
     public static String greet(String language){
-        // your code
-        return null;
+        Map<String,String> welcomeMap = new HashMap<>();
+        welcomeMap.put("english", "Welcome");
+        welcomeMap.put("czech", "Vitejte");
+        welcomeMap.put("danish", "Velkomst");
+        welcomeMap.put("dutch", "Welkom");
+        welcomeMap.put("estonian", "Tere tulemast");
+        welcomeMap.put("finnish", "Tervetuloa");
+        welcomeMap.put("flemish", "Welgekomen");
+        welcomeMap.put("french", "Bienvenue");
+        welcomeMap.put("german", "Willkommen");
+        welcomeMap.put("irish", "Failte");
+        welcomeMap.put("italian", "Benvenuto");
+        welcomeMap.put("latvian", "Gaidits");
+        welcomeMap.put("lithuanian", "Laukiamas");
+        welcomeMap.put("polish", "Witamy");
+        welcomeMap.put("spanish", "Bienvenido");
+        welcomeMap.put("swedish", "Valkommen");
+        welcomeMap.put("welsh", "Croeso");
+
+        return welcomeMap.get(language);
     }
 }
