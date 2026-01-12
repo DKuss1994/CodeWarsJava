7d5007eaf0b4f3fbe43a0f02d5eb8aa07b4673f8
DKuss1994 <kussdennis@web.de>
2026-01-12 11:03:41 +0100

    new kata testcase failed

---
+++ src/main/java/kyu8/numberToString/numberToString.java
@@ -0,0 +1,7 @@
+package kyu8.numberToString;
+
+class numberToString {
+    public static String numberToString(int num) {
+        return ""; // Return a string of the number here!
+    }
+}
---
+++ src/test/java/kyu8/numberToString/numberToStringTest.java
@@ -0,0 +1,11 @@
+package kyu8.numberToString;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class numberToStringTest {
+    @Test
+    void testCase(){
+        Assertions.assertEquals("0", numberToString.numberToString(0));
+    }
+}
