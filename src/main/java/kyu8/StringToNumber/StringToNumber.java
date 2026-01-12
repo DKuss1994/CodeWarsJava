e111a90acd6895884f4033feafe4edfc49053de8
DKuss1994 <kussdennis@web.de>
2026-01-12 10:52:35 +0100

    week update

--- src/main/java/kyu8/StringToNumber/StringToNumber.java
+++ src/main/java/kyu8/StringToNumber/StringToNumber.java
@@ -1,4 +1,8 @@
 package kyu8.StringToNumber;
 
 public class StringToNumber {
+    public static int stringToNumber(String str) {
+        //TODO: Convert str into a number
+        return 1;
+    }
 }
--- src/test/java/kyu8/StringToNumber/StringToNumberTest.java
+++ src/test/java/kyu8/StringToNumber/StringToNumberTest.java
@@ -1,4 +1,11 @@
 package kyu8.StringToNumber;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StringToNumberTest {
+    @Test
+    void testCase1(){
+        Assertions.assertEquals(0,StringToNumber.stringToNumber("0ckou"));
+    }
 }
