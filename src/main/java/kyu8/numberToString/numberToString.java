334f72347e8acd9e284854448f00d8be5bdfa214
DKuss1994 <kussdennis@web.de>
2026-01-12 11:04:55 +0100

    kata solved

--- src/main/java/kyu8/numberToString/numberToString.java
+++ src/main/java/kyu8/numberToString/numberToString.java
@@ -2,6 +2,6 @@ package kyu8.numberToString;
 
 class numberToString {
     public static String numberToString(int num) {
-        return ""; // Return a string of the number here!
+        return String.valueOf(num);
     }
 }
