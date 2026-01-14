f277b0e4d4a3aaa1cbe5276ab6eded709d7262e2
DKuss1994 <kussdennis@web.de>
2026-01-14 21:59:39 +0100

    the basic tests failed

--- src/test/java/kyu8/Multiplication_table/KataTest.java
+++ src/test/java/kyu8/Multiplication_table/KataTest.java
@@ -1,4 +1,14 @@
 package kyu8.Multiplication_table;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class KataTest {
+    @Test
+    public void basicTests() {
+        String a = "1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50";
+        Assertions.assertEquals(a,Kata.multiTable(5));
+        String b = "1 * 1 = 1\n2 * 1 = 2\n3 * 1 = 3\n4 * 1 = 4\n5 * 1 = 5\n6 * 1 = 6\n7 * 1 = 7\n8 * 1 = 8\n9 * 1 = 9\n10 * 1 = 10";
+        Assertions.assertEquals(b,Kata.multiTable(1));
+    }
 }
