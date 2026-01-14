c72953d72aa7589ab9925a48fbe4bad6d9ee20ae
DKuss1994 <kussdennis@web.de>
2026-01-14 21:55:42 +0100

    new kata

---
+++ src/main/java/kyu8/Multiplication_table/Kata.java
@@ -0,0 +1,7 @@
+package kyu8.Multiplication_table;
+
+class Kata {
+    public static String multiTable(int num) {
+        return ""; // good luck
+    }
+}
---
+++ src/test/java/kyu8/Multiplication_table/KataTest.java
@@ -0,0 +1,4 @@
+package kyu8.Multiplication_table;
+
+public class KataTest {
+}
