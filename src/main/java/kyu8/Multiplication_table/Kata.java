d0f3f1b6feee4a1510fcd4e8104f9ae99c35fbde
DKuss1994 <kussdennis@web.de>
2026-01-14 22:14:31 +0100

    solved kata

--- src/main/java/kyu8/Multiplication_table/Kata.java
+++ src/main/java/kyu8/Multiplication_table/Kata.java
@@ -2,6 +2,16 @@ package kyu8.Multiplication_table;
 
 class Kata {
     public static String multiTable(int num) {
-        return ""; // good luck
+        StringBuilder x = new StringBuilder();
+        for (int i = 1; i <= 10; i++) {
+            x.append(i).append(" * ").append(num).append(" = ").append(i * num);
+            if(i==10){
+                break;
+            }
+            x.append(System.lineSeparator());
+
+        }
+        System.out.println(x.toString());
+        return x.toString(); // good luck
     }
 }
