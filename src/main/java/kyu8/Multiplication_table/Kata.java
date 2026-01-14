862414d48a5a418cf53be6d647ea5816d4e0a7de
DKuss1994 <kussdennis@web.de>
2026-01-14 22:22:53 +0100

    readme

--- src/main/java/kyu8/Multiplication_table/Kata.java
+++ src/main/java/kyu8/Multiplication_table/Kata.java
@@ -1,4 +1,50 @@
 package kyu8.Multiplication_table;
+/*# Multiplication table for number (8 kyu)
+
+# https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
+# Sprache: Java
+# Datum: 12.01.2026
+
+# Aufgabe:
+# Schreibe eine Funktion, die für eine Zahl von 1 bis 10
+# eine komplette Einmaleins Tabelle als String zurückgibt
+
+# Die Tabelle soll von 1 bis 10 gehen
+# Jede Zeile folgt dem Format:
+# i * num = ergebnis
+
+# Wichtig:
+# Zeilen werden durch Zeilenumbrüche getrennt
+# Am Ende darf kein zusätzlicher Zeilenumbruch stehen
+
+# Eingabe:
+# num   Ganzzahl zwischen 1 und 10
+
+# Ausgabe:
+# Ein String mit der vollständigen Multiplikationstabelle
+
+# Beispiel für num = 5:
+# 1 * 5 = 5
+# 2 * 5 = 10
+# ...
+# 10 * 5 = 50
+
+# Lösungsansatz 1:
+# Nutzung eines StringBuilders
+# Schleife von 1 bis 10
+# Nach jeder Zeile wird ein Zeilenumbruch hinzugefügt
+# Beim letzten Durchlauf wird kein Umbruch mehr ergänzt
+
+# Lösungsansatz 2:
+# Nutzung eines StringBuilders mit festem Zeilenumbruch
+# Nach der Schleife wird der letzte Umbruch mit trim entfernt
+# Dadurch entsteht kein trailing newline
+
+# Ich habe gelernt:
+# wie man formatierte Strings zusammensetzt
+# wie man StringBuilder effizient nutzt
+# warum sauberes Zeilenhandling wichtig für Tests ist
+*/
 
 class Kata {
     public static String multiTable(int num) {
@@ -11,17 +57,16 @@ class Kata {
             x.append(System.lineSeparator());
 
         }
-        System.out.println(x.toString());
-        return x.toString(); // good luck
+        return x.toString();
     }
     public static String multiTable2(int num){
         StringBuilder stringBuilder = new StringBuilder();
         int result;
         for (int i = 1; i <= 10; i++) {
             result=i*num;
-            stringBuilder.append(i+" * "+num+" = "+result+"\n");
+            stringBuilder.append(i).append(" * ").append(num).append(" = ").append(result).append("\n");
         }
-        System.out.println(stringBuilder);
+
         return stringBuilder.toString().trim();
     }
 }
