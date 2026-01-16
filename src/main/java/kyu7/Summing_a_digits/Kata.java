7d0bc318d11e99f549189ff6b1e30cfcf295aaa8
DKuss1994 <kussdennis@web.de>
2026-01-16 18:31:49 +0100

    solved

--- src/main/java/kyu7/Summing_a_digits/Kata.java
+++ src/main/java/kyu7/Summing_a_digits/Kata.java
@@ -2,6 +2,16 @@ package kyu7.Summing_a_digits;
 
 public class Kata{
     public static int sumDigits(int number){
-        return number;
+        String x = Integer.toString(number);
+        char[] charArray = x.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if(charArray[i]== '-'){
+                continue;
+            }
+            sum+=Integer.parseInt( String.valueOf(charArray[i]));
+        }
+
+        return sum;
     }
 }
