fa46cd908572c037353d2381a42ab58affa6fb58
DKuss1994 <kussdennis@web.de>
2026-01-28 22:12:33 +0100

    what i learn

--- src/main/java/kyu8/Set_Alarm/Alarm.java
+++ src/main/java/kyu8/Set_Alarm/Alarm.java
@@ -1,12 +1,46 @@
 package kyu8.Set_Alarm;
+/*# L1 Set Alarm (8 kyu)
+
+# https://www.codewars.com/kata/568dcc3c7f12767a62000038
+# Sprache: Java
+# Datum: 28.01.2026
+
+# Aufgabe:
+# Schreibe eine Funktion die entscheidet ob ein Wecker gestellt werden soll
+# Es gibt zwei Bedingungen
+# Ob man arbeitet
+# Ob man im Urlaub ist
+
+# Logik:
+# Der Wecker soll nur dann gestellt werden
+# Wenn man arbeitet
+# Und gleichzeitig nicht im Urlaub ist
+
+# Wahrheitstabelle:
+# employed true vacation true  -> false
+# employed true vacation false -> true
+# employed false vacation true -> false
+# employed false vacation false -> false
+
+# Lösungsansatz:
+# Nutzung eines logischen UND Operators
+# Zusätzliches Negieren des Urlaubsstatus
+# Rückgabe des direkten booleschen Ausdrucks ohne zusätzliche if Abfragen
+
+# Code:
+# return employed && !vacation;
+
+# Lernpunkte:
+# Sicherer Umgang mit boolescher Logik
+# Verständnis von logischen Operatoren
+# Reduktion von Code auf eine klare und lesbare Aussage
+# Schreiben präziser Unit Tests
+*/
 
 public class Alarm {
 
     public static boolean setAlarm(boolean employed, boolean vacation) {
-        if(employed && !vacation){
-            return true;
-        }
-       return false;
+        return employed && !vacation;
     }
 
 }
--- src/test/java/kyu8/Set_Alarm/AlarmTest.java
+++ src/test/java/kyu8/Set_Alarm/AlarmTest.java
@@ -3,16 +3,13 @@ package kyu8.Set_Alarm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 public class AlarmTest {
 
     @Test
     public void setAlarmTest() {
-        Assertions.assertTrue(Alarm.setAlarm(true, false), "Should be true.");
-        Assertions.assertFalse(Alarm.setAlarm(true, true), "Should be false.");
-        Assertions.assertFalse(Alarm.setAlarm(false, false), "Should be false.");
-        Assertions.assertFalse(Alarm.setAlarm(false, true), "Should be false.");
+       Assertions.assertTrue(Alarm.setAlarm(true,false));
+       Assertions.assertFalse(Alarm.setAlarm(false,true));
+       Assertions.assertFalse(Alarm.setAlarm(false,false));
+       Assertions.assertFalse(Alarm.setAlarm(true,true));
     }
 }
