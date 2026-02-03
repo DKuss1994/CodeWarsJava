2d209abf1c9a27c9cd7764955c6b915a38f35075
DKuss1994 <kussdennis@web.de>
2026-02-03 18:44:28 +0100

    new kata helpAlex test Failed

---
+++ src/main/java/kyu8/HelpAlex/HelpAlex.java
@@ -0,0 +1,7 @@
+package kyu8.HelpAlex;
+
+public class HelpAlex{
+    public static String hoopCount(int n){
+        return null;
+    }
+}
---
+++ src/test/java/kyu8/HelpAlex/HelpAlexTest.java
@@ -0,0 +1,14 @@
+package kyu8.HelpAlex;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelpAlexTest {
+    @Test
+    public void testHoopCount(){
+        Assertions.assertEquals("Great, now move on to tricks", HelpAlex.hoopCount(11));
+        Assertions.assertEquals("Keep at it until you get it", HelpAlex.hoopCount(7));
+    }
+}
