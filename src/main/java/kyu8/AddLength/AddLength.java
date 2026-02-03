61d7f96e403592a594b39a7171b7cf4bc5a4fa76
DKuss1994 <kussdennis@web.de>
2026-02-03 10:25:17 +0100

    solved

--- src/main/java/kyu8/AddLength/AddLength.java
+++ src/main/java/kyu8/AddLength/AddLength.java
@@ -3,7 +3,24 @@ package kyu8.AddLength;
 public class AddLength{
 
     public static String[] addLength(String str){
-        return new String[0];
+        String[] x = getStrings(str);
+        if (x != null) return x;
+        return getStrings1(str);
+    }
+
+    private static String[] getStrings1(String str) {
+        String []strList = str.split(" ");
+        for (int i = 0; i < strList.length; i++) {
+            strList[i] += " "+strList[i].length();
+        }
+        return strList;
+    }
+
+    private static String[] getStrings(String str) {
+        if(str.isEmpty()){
+            return new String[]{"0"};
+        }
+        return null;
     }
 
 }
