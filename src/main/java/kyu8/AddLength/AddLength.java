75ff00fac77a4796ed00347047cffbe32ca40de3
DKuss1994 <kussdennis@web.de>
2026-02-03 10:17:22 +0100

    addLength test failed

---
+++ src/main/java/kyu8/AddLength/AddLength.java
@@ -0,0 +1,9 @@
+package kyu8.AddLength;
+
+public class AddLength{
+
+    public static String[] addLength(String str){
+        return new String[0];
+    }
+
+}
---
+++ src/test/java/kyu8/AddLength/AddLengthTest.java
@@ -0,0 +1,17 @@
+package kyu8.AddLength;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AddLengthTest {
+    @Test
+    void testCase1(){
+        String[] test = {"ban 3"};
+        Assertions.assertArrayEquals(test,AddLength.addLength("ban"));
+    }
+    @Test
+    void testCase2(){
+        String[] test = {"0"};
+        Assertions.assertArrayEquals(test,AddLength.addLength(""));
+    }
+}
