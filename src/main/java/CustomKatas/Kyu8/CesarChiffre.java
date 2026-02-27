82b8a44afa6a5b0c99a114f008ebf18219c1a302
DKuss1994 <kussdennis@web.de>
2026-02-27 12:00:45 +0100

    kata is now on Codewars

--- src/main/java/CustomKatas/Kyu8/CesarChiffre.java
+++ src/main/java/CustomKatas/Kyu8/CesarChiffre.java
@@ -1,7 +1,75 @@
 package CustomKatas.Kyu8;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CesarChiffre {
-    public static String decryption(String code,int salt) {
-        return null;
+    public static String decryption(String code, int salt) {
+        char[] codeChar = code.toCharArray();
+        Map<Character, Integer> cipherBigChar = new HashMap<>();
+        Map<Character, Integer> cipherLittleChar = new HashMap<>();
+        StringBuilder decryptionCode = new StringBuilder();
+        int asciiValueBig = 65;
+        int asciiValueLittle = 97;
+        createMap(asciiValueBig, cipherBigChar);
+        createMap(asciiValueLittle, cipherLittleChar);
+        for (char c : codeChar) {
+            if (c == ' ') {
+                decryptionCode.append(" ");
+            }
+            if (cipherBigChar.containsKey(c)) {
+                Integer valueToFind = cipherBigChar.get(c);
+                valueToFind = descrptionKey(salt, valueToFind);
+                Character key = getCharacter(cipherBigChar, valueToFind);
+
+
+                decryptionCode.append(key);
+
+
+            }
+            if (cipherLittleChar.containsKey(c)) {
+                Integer valueToFind = cipherLittleChar.get(c);
+                valueToFind = descrptionKey(salt, valueToFind);
+                Character key = getCharacter(cipherLittleChar, valueToFind);
+
+
+                decryptionCode.append(key);
+
+
+            }
+        }
+        return String.valueOf(decryptionCode);
     }
+
+    private static Integer descrptionKey(int salt, Integer valueToFind) {
+        for (int i1 = 0; i1 < salt; i1++) {
+            valueToFind--;
+            if (valueToFind < 1) {
+                valueToFind = 26;
+            }
+            if (valueToFind > 26) {
+                valueToFind = 1;
+            }
+        }
+        return valueToFind;
+    }
+
+    private static Character getCharacter(Map<Character, Integer> cipherBigChar, Integer valueToFind) {
+        return cipherBigChar.entrySet().stream()
+                .filter(entry -> valueToFind.equals(entry.getValue()))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private static void createMap(int asciiValueBig, Map<Character, Integer> cipherBigChar) {
+        for (int i = 1; i <= 26; i++) {
+            char charBig = (char) asciiValueBig;
+            cipherBigChar.put(charBig, i);
+            asciiValueBig++;
+
+        }
+    }
+
 }
+
--- src/test/java/CustomKatas/Kyu8/CesarChiffreTest.java
+++ src/test/java/CustomKatas/Kyu8/CesarChiffreTest.java
@@ -9,4 +9,85 @@ public class CesarChiffreTest {
         Assertions.assertEquals("Hello",CesarChiffre.decryption("Ifmmp",1));
 
     }
+    @Test
+    void testBig1(){
+        Assertions.assertEquals("HELLO",CesarChiffre.decryption("IFMMP",1));
+
+    }
+    @Test
+    void test2(){
+        Assertions.assertEquals("Z",CesarChiffre.decryption("A",1));
+
+    }
+    @Test
+    void test3(){
+        Assertions.assertEquals("A",CesarChiffre.decryption("A",26));
+
+    }
+    @Test
+    void test4(){
+        Assertions.assertEquals("A",CesarChiffre.decryption("A",52));
+
+    }
+
+    @Test
+    void test5(){
+        Assertions.assertEquals("H e l l o",CesarChiffre.decryption("I f m m p",1));
+
+    }
+    @Test
+    void test6(){
+        Assertions.assertEquals("BA",CesarChiffre.decryption("C?B",1));
+
+    }
+    @Test
+    void testLittle1(){
+        Assertions.assertEquals("hello",CesarChiffre.decryption("ifmmp",1));
+
+    }
+    @Test
+    void testLittle2(){
+        Assertions.assertEquals("z",CesarChiffre.decryption("a",1));
+
+    }
+    @Test
+    void testLittle3(){
+        Assertions.assertEquals("a",CesarChiffre.decryption("a",26));
+
+    }
+    @Test
+    void testLittle4(){
+        Assertions.assertEquals("a",CesarChiffre.decryption("a",52));
+
+    }
+    @Test
+    void testLittle5(){
+        Assertions.assertEquals("ba",CesarChiffre.decryption("c?b",1));
+
+    }
+    @Test
+    void testMix1(){
+        Assertions.assertEquals("hElLo",CesarChiffre.decryption("iFmMp",1));
+
+    }
+    @Test
+    void testMix2(){
+        Assertions.assertEquals("zZz",CesarChiffre.decryption("aAa",1));
+
+    }
+    @Test
+    void testMix3(){
+        Assertions.assertEquals("aAaA",CesarChiffre.decryption("aAaA",26));
+
+    }
+    @Test
+    void testMix4(){
+        Assertions.assertEquals("aAaA",CesarChiffre.decryption("aAaA",52));
+
+    }
+    @Test
+    void testMix5(){
+        Assertions.assertEquals("BaBa",CesarChiffre.decryption("C?b!C,b",1));
+
+    }
 }
